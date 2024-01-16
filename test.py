16ad044c0bd80d0ff9f4f37fff3e5bce057d95cf
kup <konka.um@gmail.com>
2024-01-16 18:03:11 +0700

    fuck me idk what i just did

---
+++ test.py
@@ -0,0 +1,19 @@
+from math import log2
+
+# b = 1
+# c = 0.5
+
+# for i in range(20):
+#     b += c
+#     print(b)
+#     c = c / 2
+
+# print(b ** 2)
+
+a = 0.1
+b = 0.01
+c = 0.001
+
+print(log2(a ** 2))
+print(log2(b ** 2))
+print(log2(c ** 2))
--- week2 hw.py
+++ week2 hw.py
@@ -1,4 +1,4 @@
-e = 0.1
+#e = 0.001
 
 def mystery(x, a, b):
     c = (a + b) / 2
@@ -11,4 +11,26 @@ def mystery(x, a, b):
     else:
         return mystery(x, c, b)
     
-print(mystery(1, 1, 1))
+#print(mystery(7, 2, 3))
+
+# try to find time complexity (plz send help)
+
+def mystery_debug(x, a, b, e, count = 0):
+    c = (a + b) / 2
+    d = c**2 - x
+    count += 1
+
+    if abs(d) <= e:
+        return c, count
+    elif d > e:
+        return mystery_debug(x, a, c, e, count)
+    else:
+        return mystery_debug(x, c, b, e, count)
+    
+print(mystery_debug(3.999996185303644, 1, 2, 0.1))
+print(mystery_debug(3.999996185303644, 1, 2, 0.01))
+print(mystery_debug(3.999996185303644, 1, 2, 0.0001))
+
+print(mystery_debug(2, 1, 2, 0.1))
+print(mystery_debug(2, 1, 2, 0.01))
+print(mystery_debug(2, 1, 2, 0.0001))
