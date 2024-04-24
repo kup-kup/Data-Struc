cdd79f8a5291e8c9fed01d231bbe33e9be1f6cd7
kup <konka.um@gmail.com>
2024-04-24 20:26:33 +0700

    init main.py in final_ติว

---
+++ final_ติว/tree.py
@@ -0,0 +1,9 @@
+class Tree:
+  def __init__(self, root = None):
+    self.root = root
+
+class TreeNode:
+  def __init__(self, data=None, left = None, right = None):
+    self.data = data
+    self.left = left
+    self.right = right
